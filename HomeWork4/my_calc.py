0a66739c69e0f7737e0ec7389bd2844eb9fad4b5
@t.poleshko <t.poleshko@xpay.by>
2023-03-20 15:42:00 +0300

    Add try/except for zero

--- HomeWork4/my_calc.py
+++ HomeWork4/my_calc.py
@@ -1,11 +1,17 @@
 def func_sum(number1, number2):
     return number1 + number2
 
+
 def func_minus(number1, number2):
     return number1 - number2
+
 
 def func_increase(number1, number2):
     return number1 * number2
 
+
 def func_division(number1, number2):
-    return number1 / number2
+    try:
+        return number1 / number2
+    except ZeroDivisionError:
+        print('Делить на ноль нельзя')
