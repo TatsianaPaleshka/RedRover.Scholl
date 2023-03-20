8b5f8906d49e041bf7ef8331924dd8d6daefeacb
@t.poleshko <t.poleshko@xpay.by>
2023-03-20 16:15:50 +0300

    Add input numbers

--- HomeWork4/HomeWork4_6.py
+++ HomeWork4/HomeWork4_6.py
@@ -1,7 +1,8 @@
 # 4.6. Создайте файл my_calc.py и пропишите в нем минимум 4 функции, выполняющие базовые арифметические вычисления.
 #      Примените эти функции в качестве методов в другом файле.
 import my_calc
-print(f'Сложение чисел: {my_calc.func_sum(3, 5)}')
-print(f'Вычитание чисел: {my_calc.func_minus(3, 5)}')
-print(f'Умножение чисел: {my_calc.func_increase(3, 5)}')
-print(f'Деление чисел: {my_calc.func_division(3, 5)}')
+num1, num2 = int(input('Введите число 1: ')), int(input('Введите число 2: ')),
+print(f'Сложение чисел: {my_calc.func_sum(num1, num2)}')
+print(f'Вычитание чисел: {my_calc.func_minus(num1, num2)}')
+print(f'Умножение чисел: {my_calc.func_increase(num1, num2)}')
+print(f'Деление чисел: {my_calc.func_division(num1, num2)}')
